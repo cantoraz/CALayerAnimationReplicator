360cf7e5c2e45690ddca800226ee2265b03be10c
Cantoraz Chou <cantoraz@gmail.com>
2016-07-12 09:16:34 +0800

    Initial Commit

---
+++ CALayerAnimationReplicator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CALayerAnimationReplicator
+//
+//  Created by Cantoraz Chou on 7/12/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
