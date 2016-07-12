41af74655e633711a7de42d5073191399c827f66
Cantoraz Chou <cantoraz@gmail.com>
2016-07-12 12:22:40 +0800

    ADDED: Add an activity indicator animation.

--- CALayerAnimationReplicator/ViewController.m
+++ CALayerAnimationReplicator/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 @property (nonatomic, weak) IBOutlet UIView* replicatorAnimationView;
+@property (weak, nonatomic) IBOutlet UIView* activityIndicatorView;
 @end
 
 @implementation ViewController
@@ -48,6 +49,45 @@
     replicatorLayer.instanceTransform = CATransform3DMakeTranslation(31, 0, 0);
     replicatorLayer.instanceDelay = 0.3;
     replicatorLayer.masksToBounds = true;
+    
+    //--------------------Activity-Indicator--------------------
+
+    // Prepare a CALayer replicator
+    CAReplicatorLayer* activityReplicatorLayer = [[CAReplicatorLayer alloc] init];
+    activityReplicatorLayer.bounds = _activityIndicatorView.bounds;
+    activityReplicatorLayer.position = CGPointMake(_activityIndicatorView.bounds.size.width/2,
+                                                   _activityIndicatorView.bounds.size.height/2);
+//    activityReplicatorLayer.backgroundColor = [UIColor blackColor].CGColor;
+    activityReplicatorLayer.backgroundColor = [UIColor clearColor].CGColor;
+    [_activityIndicatorView.layer addSublayer:activityReplicatorLayer];
+    
+    // Prepare a prototype CALayer to be replicated
+    CALayer* circle = [[CALayer alloc] init];
+    circle.bounds = CGRectMake(0, 0, 10, 10);
+    circle.position = CGPointMake(activityReplicatorLayer.bounds.size.width/2,
+                                  activityReplicatorLayer.bounds.size.height/2 - 40);
+    circle.cornerRadius = 5;
+    circle.backgroundColor = [UIColor whiteColor].CGColor;
+    [activityReplicatorLayer addSublayer:circle];
+
+    // Replicate the prototype CALayer in the replicator CALayer
+    activityReplicatorLayer.instanceCount = 15;
+    CGFloat angle = 2 * M_PI / activityReplicatorLayer.instanceCount;
+    activityReplicatorLayer.instanceTransform = CATransform3DMakeRotation(angle, 0, 0, 1);
+    
+    // Add animation to the prototype CALayer
+    CABasicAnimation* scale = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scale.fromValue = @1;
+    scale.toValue = @0;
+    scale.duration = 1;
+    scale.repeatCount = HUGE;
+    [circle addAnimation:scale forKey:nil];
+    
+    // Set delay between replicated instances
+    activityReplicatorLayer.instanceDelay = 1./15;
+    
+    // Set the initial scale
+    circle.transform = CATransform3DMakeScale(0, 0, 1);
 }
 
 - (void)didReceiveMemoryWarning {
